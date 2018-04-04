14d82e8d87d4ac32f5af598ff2261121972014a2
code302 <38006689+code302@users.noreply.github.com>
2018-04-04 21:20:02 +0800

    Update Softether-x64.sh

--- Debian/Softether-x64.sh
+++ Debian/Softether-x64.sh
@@ -14,8 +14,8 @@ apt-get update -y
 apt-get install build-essential -y
 #Get the Softether Packages via wget and Save it into /root
 cd /root
-wget http://www.softether-download.com/files/softether/v4.10-9473-beta-2014.07.12-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.10-9473-beta-2014.07.12-linux-x64-64bit.tar.gz
-tar zxf softether-vpnserver-v4.10-9473-beta-2014.07.12-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.25-9656-rtm-2018.01.15-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz
+tar xzvf softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz
 #Making files, manual input needed
 cd vpnserver
 clear
@@ -30,7 +30,7 @@ chmod 700 vpncmd
 chmod 700 vpnserver
 #Downloading the Scripts for init.d and set Permission
 cd /root
-wget https://raw.githubusercontent.com/mfaishalakbar/SoftetherAutoInstall/master/misc/vpnserver-debian.sh --no-check-certificate
+wget https://raw.githubusercontent.com/code302/fdfdfdfsf/master/misc/vpnserver-debian.sh --no-check-certificate
 mv vpnserver-debian.sh /etc/init.d/vpnserver
 cd /etc/init.d
 chmod 755 vpnserver
